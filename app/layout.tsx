1dbd17a3248ee74ff34a029c8fded0ac3578565b
Shaun Fernandes <145692712+shaunfernandes84@users.noreply.github.com>
2024-05-12 03:38:12 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shaun's_VC",
+  title: "ShaunF_VC",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
